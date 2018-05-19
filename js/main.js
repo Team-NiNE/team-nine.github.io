7ebeb2eb34f1c7270f3adf3e71e2b1f4e4223237
Basix <jsrtyu1120@gmail.com>
2018-05-20 04:06:54 +0900

    슬라이드와 네비게이션이 함께 움직이도록 수정 (스펙에 맞게)

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ $(function() {
         onLeave: function(index, nextIndex, direction){
             var leavingSection = $(this);
             if(index === 1 && direction === 'down') {
-                $('#menu').css('left', '0.1em')
+                $('#menu').css('bottom', '50vh')
             }
             if(nextIndex === 1 && direction === 'up') {
-                $('#menu').css('left', '-10em')
+                $('#menu').css('bottom', '-50vh')
             }
             $('.slide-indicator-'+index+'>i').removeClass('fas').addClass('far')
             $('.slide-indicator-'+nextIndex+'>i').removeClass('far').addClass('fas')
