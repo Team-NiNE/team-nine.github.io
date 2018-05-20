4b03dab5e1e99e1a5cc8f9a57cab5bec7fac2dff
Basix <jsrtyu1120@gmail.com>
2018-05-20 19:39:56 +0900

    config.js 생성 및 연결

---
+++ js/config.js
@@ -0,0 +1,23 @@
+members = [
+    {
+        nickname: 'Basix',
+        position: 'Developer',
+        about: 'I am Basix.',
+        color: '#46bfec', // CSS color
+        images: { //image url
+            profile: '', 
+            banner: '' // optional
+        },
+        social: { // optional
+            twitter: 'https://twitter.com/Basix1120' // Font Awesome의 아이콘 코드와 Full URL
+        }
+    }
+]
+projects = [
+    {
+        name: 'Team.NiNE Website',
+        description: 'The website.',
+        technology: ['html5', 'js', 'css3-alt'], // Font Awesome의 아이콘 코드
+        github: 'https://github.com/Team-NiNE/team-nine.github.io' // optional
+    }
+]
