281a4797d506bcd8f84316cb6f117c6a225e774f
Basix <jsrtyu1120@gmail.com>
2018-05-22 04:37:04 +0900

    더미용 데이터 추가

--- js/config.js
+++ js/config.js
@@ -5,11 +5,37 @@ members = [
         about: 'I am Basix.',
         color: '#46bfec', // CSS color
         images: { //image url
-            profile: '', 
+            profile: 'https://www.gravatar.com/avatar/bdc333bbab37277c58cba9d9e0f032c6?s=512', 
             banner: '' // optional
         },
         social: { // optional
             twitter: 'https://twitter.com/Basix1120' // Font Awesome의 아이콘 코드와 Full URL
+        }
+    },
+    {
+        nickname: 'Kiwi',
+        position: 'Developer',
+        about: 'Cute Hentai Developer',
+        color: '#9CE159', // CSS color
+        images: { //image url
+            profile: 'https://apteryx.moe/img/avatar-icon.png', 
+            banner: 'https://pbs.twimg.com/media/Dds2EL7U0AIDi3-.jpg' // optional
+        },
+        social: { // optional
+            twitter: 'https://twitter.com/kdo9921' // Font Awesome의 아이콘 코드와 Full URL
+        }
+    },
+    {
+        nickname: 'Cocoa',
+        position: 'Developer',
+        about: '집에 가고 싶습니다',
+        color: '#FFC0CB', // CSS color
+        images: { //image url
+            profile: 'https://hoto.us/image512.jpg', 
+            banner: '' // optional
+        },
+        social: { // optional
+            twitter: 'https://twitter.com/_Hoto_Cocoa_' // Font Awesome의 아이콘 코드와 Full URL
         }
     }
 ]
